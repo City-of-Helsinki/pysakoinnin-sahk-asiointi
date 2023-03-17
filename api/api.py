b2d8abbc23d7ddbab8fe072931e6013aeebd93bf
jihanm <jaakko.ihanamaki@futurice.com>
2023-03-17 11:16:26 +0200

    Add PASI unit tests

--- api/api.py
+++ api/api.py
@@ -7,8 +7,8 @@ router = Router()
 
 
 class FoulRequest(Schema):
-    foul_number: int = 113148427
-    register_number: str = "HKR-999"
+    foul_number: int
+    register_number: str
 
 
 class NotFoundError(Schema):
@@ -20,40 +20,40 @@ def helloworld(request):
     return {"msg": 'Hello world'}
 
 
-@router.get('/getFoulData', response={200: FoulDataResponse, 404: None}, tags=['PASI'])
+@router.get('/getFoulData', response={200: FoulDataResponse, 404: NotFoundError}, tags=['PASI'])
 def get_foul_data(request, foul_number: int = 113148427, register_number: str = "HKR-999"):
     """
     Retrieve foul data from PASI by foul number and register number
     """
     req = PASIHandler.get_foul_data(foul_number, register_number)
-    return req
+    return req.json()
 
 
-@router.get('/getTransferData', response={200: TransferDataResponse, 404: None}, tags=['PASI'])
+@router.get('/getTransferData', response={200: TransferDataResponse, 404: NotFoundError}, tags=['PASI'])
 def get_transfer_data(request, transfer_number: int = 11720143, register_number: str = "HKR-999"):
     """
     Retrieve transfer data from PASI by transfer number and register number
     """
     req = PASIHandler.get_transfer_data(transfer_number, register_number)
-    return req
+    return req.json()
 
 
-@router.post('/extendDueDate', response={200: ExtendDueDateResponse}, tags=['PASI'])
+@router.post('/extendDueDate', response={200: ExtendDueDateResponse, 422: None}, tags=['PASI'])
 def extend_due_date(request, foul_data: FoulRequest):
     """
     Extend foul due date by 30 days
     """
     req = PASIHandler.extend_foul_due_date(foul_data)
-    return req
+    return req.json()
 
 
-@router.post('/saveObjection', response={204: None, 422: None}, tags=['PASI'])
+@router.post('/saveObjection', response={200: None, 204: None, 422: None}, tags=['PASI'])
 def save_objection(request, objection: Objection):
     """
     Send a new objection to PASI
     """
     req = PASIHandler.save_objection(objection)
-    return req
+    return req.json()
 
 
 @router.get('/getDocuments/{user_id}', response={200: ATVDocumentResponse}, tags=['ATV'])
@@ -62,7 +62,7 @@ def get_atv_documents(request, user_id: str):
     Retrieve all user documents from ATV with UUID
     """
     req = ATVHandler.get_documents(user_id)
-    return req
+    return req.json()
 
 
 @router.get('/getDocumentByTransactionId/{id}', response={200: ATVDocumentResponse, 404: NotFoundError, 500: None},
@@ -72,7 +72,7 @@ def get_document_by_transaction_id(request, id):
     Get document from ATV by foul ID
     """
     req = ATVHandler.get_document_by_transaction_id(id)
-    return req
+    return req.json()
 
 
 @router.post('/sendObjection/{foul_id}',
@@ -83,10 +83,10 @@ def send_objection_to_atv(request, objection: Objection, foul_id: str):
     Upload new user document to ATV
     """
     req = ATVHandler.add_document(objection, foul_id)
-    return req
+    return req.json()
 
 
-@router.patch('/setDocumentStatus', response={200: None, 401: None, 404: None, 422: None},
+@router.patch('/setDocumentStatus', response={200: None, 401: None, 404: NotFoundError, 422: None},
               tags=['Pysaköinnin asiointi'])
 def set_document_status(request, status_request: DocumentStatusRequest):
     """
