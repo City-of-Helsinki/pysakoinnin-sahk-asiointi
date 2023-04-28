c83d5770d288a6d9897617cd554075f95c777366
Jaakko Ihanamäki <84900656+jihanm@users.noreply.github.com>
2023-04-28 11:32:30 +0300

    Debug objection (#57)
    * Debug objection endpoint
    
    * Remove test ssl cert from git
    
    * Fix objection endpoint

--- api/utils.py
+++ api/utils.py
@@ -7,7 +7,7 @@ from ninja.errors import HttpError
 
 def virus_scan_attachment_file(file_data):
     try:
-        req = requests.request('POST', f"{settings.CLAMAV_HOST}", files={'FILES': base64.b64decode(file_data)})
+        req = requests.request('POST', f"{settings.CLAMAV_HOST}", files={'FILES': base64.b64decode(file_data + b'==')})
         response = req.json()
         if hasattr(response, 'data') and response['data']['result'][0]['is_infected'] is True:
             raise HttpError(422, message="File is infected")
