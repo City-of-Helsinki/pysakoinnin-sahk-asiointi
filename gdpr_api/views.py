178f659cefd45d37efac6b0cb388ad5f01cae4eb
jihanm <jaakko.ihanamaki@futurice.com>
2023-05-23 15:30:02 +0300

    Add helusers JWT auth to GDRP API

--- gdpr_api/views.py
+++ gdpr_api/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpRequest
 from environ import Env
-from helusers._oidc_auth_impl import resolve_user
-from helusers.oidc import ApiTokenAuthentication
+from helusers.jwt import JWT
+from helusers.oidc import OIDCConfig
 from ninja import Router
 from ninja.errors import HttpError
 from ninja.security import HttpBearer
@@ -13,34 +13,47 @@ router = Router()
 env = Env()
 
 
-class OIDC_API_TOKEN_AUTH():
-    def __init__(self):
-        self.AUTH_SCHEME = 'Bearer'
-        self.AUDIENCE = env('GDPR_API_AUDIENCE')
-        self.ISSUER = env('GDPR_API_ISSUER')
-        self.API_AUTHORIZATION_FIELD = env('GDPR_API_AUTH_FIELD')
-        self.API_SCOPE_PREFIX = env('TOKEN_AUTH_SCOPE_PREFIX')
-        self.USER_RESOLVER = resolve_user
-        self.REQUIRE_API_SCOPE_FOR_AUTHENTICATION = True
-        self.OIDC_CONFIG_EXPIRATION_TIME = 600
-
-
 class JWTAuth(HttpBearer):
+    def __init__(self, required_scope: str):
+        self.required_scope = required_scope
+        super().__init__()
+
     def authenticate(self, request: HttpRequest, token: str):
-        auth = ApiTokenAuthentication(settings=OIDC_API_TOKEN_AUTH())
-        result = auth.authenticate(request)
-        if result is not None:
-            return True
-        else:
-            raise HttpError(401, message="Unauthorised")
+        oidc_config = OIDCConfig(env('GDPR_API_ISSUER'))
+
+        jwt = JWT(token)
+        try:
+            issuer = jwt.issuer
+        except KeyError:
+            raise HttpError(401, message='Required "iss" claim is missing.')
+
+        if issuer != env('GDPR_API_ISSUER'):
+            raise HttpError(401, message="Unknown JWT issuer {}.".format(issuer))
+
+        try:
+            jwt.validate(oidc_config.keys(), env('GDPR_API_AUDIENCE'))
+        except Exception:
+            raise HttpError(401, message="JWT verification failed.")
+
+        api_scopes = jwt.claims.get(env('GDPR_API_AUTH_FIELD'), [])
+        if self.required_scope not in api_scopes:
+            print(self.required_scope)
+            raise HttpError(401, message="No suitable API scope found")
+
+        return jwt.claims.get("sub")
 
 
-@router.get('/{user_id}', tags=["GDPR API"], auth=JWTAuth())
-def get_user_info(request, user_id):
+@router.get('/{user_id}', tags=["GDPR API"], auth=JWTAuth(required_scope=env("GDPR_API_QUERY_SCOPE")))
+def get_user_info(request, user_id: str):
+    if not request.auth or request.auth != user_id:
+        raise HttpError(401, message="Unauthorised")
     req = ATVHandler.get_documents(user_id)
     return req
 
 
-@router.delete('/{user_id}/', tags=["GDPR API"], auth=JWTAuth())
-def delete_user_info(request):
+@router.delete('/{user_id}', tags=["GDPR API"], auth=JWTAuth(required_scope=env("GDPR_API_DELETE_SCOPE")))
+def delete_user_info(request, user_id: str):
+    if not request.auth or request.auth != user_id:
+        raise HttpError(401, message="Unauthorised")
+
     raise HttpError(403, message="Tietojen poisto ei ole mahdollista lain xyz velvoittamana")
