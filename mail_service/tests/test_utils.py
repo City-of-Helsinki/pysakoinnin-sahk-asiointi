82648ac5e069566db1e0a8521fa3e60b9387286d
henri-hietala <henri.hietala@futurice.com>
2023-08-24 12:33:46 +0300

    cleanup

--- mail_service/tests/test_utils.py
+++ mail_service/tests/test_utils.py
@@ -2,12 +2,13 @@ from django.test import TestCase
 from mail_service.utils import extend_due_date_mail_constructor, mail_constructor, headers, events
 
 mail_to = "test@email.com"
-new_due_date = "2023-01-01T00:30:00"
 
 class TestMailUtils(TestCase):
 
     def test_extend_due_date_mail_constructor(self):
-        # Test that passed information can be found in mail
+        new_due_date = "2023-01-01T00:30:00"
+
+        # test that passed information can be found in mail
         mail = extend_due_date_mail_constructor(lang="FI", new_due_date=new_due_date, mail_to=mail_to)
         assert mail.subject == headers["FI"]
         assert mail.body == "<p>Eräpäivää siirretty, uusi eräpäivä on 01.01.2023"
@@ -20,12 +21,12 @@ class TestMailUtils(TestCase):
         mail = extend_due_date_mail_constructor(lang="SV", new_due_date=new_due_date, mail_to=mail_to)
         assert mail.subject == headers["SV"]
 
-        # Upon incorrect lang, default to FI
+        # incorrect lang, default to FI
         mail = extend_due_date_mail_constructor(lang="INCORRECT_LANG", new_due_date=new_due_date, mail_to=mail_to)
         assert mail.subject == headers["FI"]
 
     def test_mail_constructor(self):
-        # Test that passed information can be found in mail
+        # test that passed information can be found in mail
         mail = mail_constructor(event="received", lang="FI", mail_to=mail_to)
         assert mail.subject == headers["FI"]
         assert events["received"]["FI"] in mail.body
@@ -38,8 +39,10 @@ class TestMailUtils(TestCase):
         mail = mail_constructor(event="received", lang="SV", mail_to=mail_to)
         assert mail.subject == headers["SV"]
 
+        # incorrect lang, default to FI
         mail = mail_constructor(event="received", lang="INCORRECT_LANG", mail_to=mail_to)
         assert mail.subject == headers["FI"]
+
         
     
        
