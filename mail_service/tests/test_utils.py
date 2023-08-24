fb790016b303692fc24c4a627c7e566c01e3b28a
henri-hietala <henri.hietala@futurice.com>
2023-08-24 10:05:24 +0300

    add different language tests

--- mail_service/tests/test_utils.py
+++ mail_service/tests/test_utils.py
@@ -15,10 +15,28 @@ class TestMailUtils(TestCase):
         assert mail.body == "<p>Eräpäivää siirretty, uusi eräpäivä on 01.01.2023"
         assert mail.to[0] == mail_to
 
-        # check that language change works
+        # check different languages
         mail = extend_due_date_mail_constructor(lang="EN", new_due_date=new_due_date, mail_to=mail_to)
         assert mail.subject == 'New event in Parking e-service'
 
         mail = extend_due_date_mail_constructor(lang="SV", new_due_date=new_due_date, mail_to=mail_to)
         assert mail.subject == 'Nya händelser i parkering e-tjänsten'
+
+        # Default should be FI thus subject should be in finnish
+        mail = extend_due_date_mail_constructor(lang=None, new_due_date=new_due_date, mail_to=mail_to)
+        assert mail.subject == "Uusi tapahtuma Pysäköinnin Asioinnissa"
+
+        # Upon incorrect lang, default to FI
+        mail = extend_due_date_mail_constructor(lang="INCORRECT_LANG", new_due_date=new_due_date, mail_to=mail_to)
+        assert mail.subject == "Uusi tapahtuma Pysäköinnin Asioinnissa"
+
+
+
+
+        
+    
+       
+
+
+    
         
