a7bc530785b810dc27f86f604676fdde844ee1f0
henri-hietala <119568415+henri-hietala@users.noreply.github.com>
2023-08-25 15:35:44 +0300

    Api function tests (#120)
    * create tests for getFouData and getTransferData
    
    * delete old broken tests
    
    * unauth tests for foulData and transferData
    
    * cleanup
    
    * add test get_atv_documents and create mock data
    
    * add test for getting docs by id
    
    * add get docs by id unauth test
    
    * delete old mock data

--- api/tests/mocks.py
+++ api/tests/mocks.py
@@ -87,6 +87,84 @@ MOCK_DUEDATE_REQUEST = {
     "register_number": "HKR-999"
 }
 
+MOCK_ATV_DOCUMENT_RESPONSE = {
+    
+  "count": 1,
+  "next": None,
+  "previous": None,
+  "results": [
+    {
+      "id": "4y11fb6c-8b25-4c3c-9ea5-e62b976667e1",
+      "created_at": "2023-06-02T13:48:59.258113+03:00",
+      "updated_at": "2023-08-03T17:38:49.303800+03:00",
+      "status": {
+        "value": "received",
+        "status_display_values": {},
+        "timestamp": "2023-06-02T13:54:20.738979+03:00",
+        "activities": []
+      },
+      "status_histories": [
+        {
+          "value": "received",
+          "status_display_values": {},
+          "timestamp": "2023-06-02T13:54:20.738979+03:00",
+          "activities": []
+        },
+        {
+          "value": "sent",
+          "status_display_values": {},
+          "timestamp": "2023-06-02T13:48:59.269477+03:00",
+          "activities": []
+        }
+      ],
+      "type": "",
+      "human_readable_type": {},
+      "service": "Pysäköinnin Sähköinen Asiointi",
+      "user_id": "uuid",
+      "transaction_id": "113148474",
+      "business_id": "",
+      "tos_function_id": "12345",
+      "tos_record_id": "12345",
+      "metadata": {
+        "foulNumber": "113148474",
+        "registerNumber": "hkr-999"
+      },
+      "content": {
+        "foulNumber": 113148474,
+        "transferNumber": None,
+        "folderID": None,
+        "ssn": "210281-9988",
+        "firstName": "Nordea",
+        "lastName": "Demo",
+        "email": "test@gmail.fi",
+        "mobilePhone": "+58254545414",
+        "bic": None,
+        "iban": "FI90 1345 1234 1234 12",
+        "authorRole": 2,
+        "address": {
+          "addressLine1": None,
+          "addressLine2": None,
+          "streetAddress": "katu 8",
+          "postCode": "00580",
+          "postOffice": "jhelksini",
+          "countryName": None
+        },
+        "description": "description",
+        "attachments": [],
+        "type": 0,
+        "sendDecisionViaEService": True,
+        "metadata": {
+          "foulNumber": "113148474",
+          "registerNumber": "hkr-999"
+        }
+      },
+      "draft": False,
+      "locked_after": None,
+      "deletable": False,
+      "attachments": []
+    }
+  ]
+}
 
 class MockResponse:
     def __init__(self, status_code, json_data):
---
+++ api/tests/test_api.py
@@ -0,0 +1,38 @@
+from unittest.mock import patch
+from django.test import TestCase
+from api.tests.mocks import MockResponse, MOCK_FOUL, MOCK_TRANSFER, MOCK_ATV_DOCUMENT_RESPONSE
+from api.api import get_foul_data, get_transfer_data, get_atv_documents, get_document_by_transaction_id
+
+class User:
+    def __init__(self, uuid):
+        self.uuid = uuid
+class Request:
+    def __init__(self, user):
+        self.user = user
+class TestApiFunctions(TestCase):
+
+    @patch('api.views.PASIHandler.get_foul_data')
+    def test_get_foul_data(self, get_foul_data_mock):
+        get_foul_data_mock.return_value = MockResponse(200, MOCK_FOUL)
+        result = get_foul_data(request=None, foul_number=MOCK_FOUL["foulNumber"], register_number=MOCK_FOUL["registerNumber"])
+        assert result == MOCK_FOUL
+
+    @patch('api.views.PASIHandler.get_transfer_data')
+    def test_get_transfer_data(self, get_transfer_data_mock):
+        get_transfer_data_mock.return_value = MockResponse(200, MOCK_TRANSFER)
+        result = get_transfer_data(request=None, transfer_number=MOCK_TRANSFER["transferNumber"], register_number=MOCK_TRANSFER["registerNumber"])
+        assert result == MOCK_TRANSFER
+    
+    @patch('api.views.ATVHandler.get_documents')
+    def test_get_atv_documents(self, get_documents_mock):
+        get_documents_mock.return_value = MockResponse(200, MOCK_ATV_DOCUMENT_RESPONSE)
+        request =  Request(user=User("fakeid"))
+        result = get_atv_documents(request=request)
+        assert result.json() == MOCK_ATV_DOCUMENT_RESPONSE
+    
+    @patch('api.views.ATVHandler.get_document_by_transaction_id')
+    def test_get_document_by_transaction_id(self, get_document_by_transaction_id_mock):
+        get_document_by_transaction_id_mock.return_value = MockResponse(200, MOCK_ATV_DOCUMENT_RESPONSE)
+        randomId = 12345 
+        result = get_document_by_transaction_id(request=None, id=randomId)
+        assert result.json() == MOCK_ATV_DOCUMENT_RESPONSE
