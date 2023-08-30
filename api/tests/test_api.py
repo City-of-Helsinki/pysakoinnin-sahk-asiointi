3e662cbf97541df774faf696843ce4a40368bec7
henri-hietala <henri.hietala@futurice.com>
2023-08-30 14:39:30 +0300

    fix test_api code smells

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -1,7 +1,7 @@
 from unittest.mock import patch
 from django.test import TestCase
 from api.tests.mocks import MockResponse, MOCK_FOUL, MOCK_TRANSFER, MOCK_ATV_DOCUMENT_RESPONSE, MOCK_DUEDATE
-from api.api import get_foul_data, get_transfer_data, get_atv_documents, get_document_by_transaction_id, save_objection, extend_due_date
+from api import api
 from ninja import Schema, errors
 
 # These classes are based on schemas defined in api.schemas
@@ -56,7 +56,9 @@ class TestApiFunctions(TestCase):
     def test_get_foul_data(self, get_foul_data_mock):
         get_foul_data_mock.return_value = MockResponse(200, MOCK_FOUL)
 
-        result = get_foul_data(request=None, foul_number=MOCK_FOUL["foulNumber"], register_number=MOCK_FOUL["registerNumber"])
+        result = api.get_foul_data(request=None,
+                                   foul_number=MOCK_FOUL["foulNumber"],
+                                   register_number=MOCK_FOUL["registerNumber"])
 
         assert result == MOCK_FOUL
 
@@ -64,27 +66,29 @@ class TestApiFunctions(TestCase):
     def test_get_transfer_data(self, get_transfer_data_mock):
         get_transfer_data_mock.return_value = MockResponse(200, MOCK_TRANSFER)
 
-        result = get_transfer_data(request=None, transfer_number=MOCK_TRANSFER["transferNumber"], register_number=MOCK_TRANSFER["registerNumber"])
+        result = api.get_transfer_data( request=None,
+                                        transfer_number=MOCK_TRANSFER["transferNumber"],
+                                        register_number=MOCK_TRANSFER["registerNumber"])
         
         assert result == MOCK_TRANSFER
-    
+
     @patch('api.views.ATVHandler.get_documents')
     def test_get_atv_documents(self, get_documents_mock):
         get_documents_mock.return_value = MockResponse(200, MOCK_ATV_DOCUMENT_RESPONSE)
 
-        result = get_atv_documents(request=self.request)
+        result = api.get_atv_documents(request=self.request)
 
         assert result.json() == MOCK_ATV_DOCUMENT_RESPONSE
-    
+
     @patch('api.views.ATVHandler.get_document_by_transaction_id')
     def test_get_document_by_transaction_id(self, get_document_by_transaction_id_mock):
         get_document_by_transaction_id_mock.return_value = MockResponse(200, MOCK_ATV_DOCUMENT_RESPONSE)
 
         randomId = 12345 
-        result = get_document_by_transaction_id(request=None, id=randomId)
+        result = api.get_document_by_transaction_id(request=None, id=randomId)
 
         assert result.json() == MOCK_ATV_DOCUMENT_RESPONSE
-    
+
     @patch('api.views.ATVHandler.add_document')
     @patch('api.views.PASIHandler.save_objection')
     def test_save_objection(self, save_objection_mock, add_document_mock):
@@ -93,7 +97,7 @@ class TestApiFunctions(TestCase):
         save_objection_mock.return_value = MockResponse(200, {})
 
         objection = Objection()
-        save_obj = lambda : save_objection(request=self.request, objection=objection)
+        save_obj = lambda : api.save_objection(request=self.request, objection=objection)
         self.assertEqual(200, save_obj())
 
         # Raise httperror 422
@@ -106,7 +110,7 @@ class TestApiFunctions(TestCase):
         objection = Objection()
         objection.metadata = "incorrect metadata"
         self.assertRaises(errors.HttpError, save_obj)
-    
+
     @patch('api.views.ATVHandler.add_document')
     @patch('api.views.PASIHandler.extend_foul_due_date')
     def test_extend_due_date(self,extend_foul_due_date_mock, add_document_mock):
@@ -114,7 +118,7 @@ class TestApiFunctions(TestCase):
         extend_foul_due_date_mock.return_value = MockResponse(200, MOCK_DUEDATE)
 
         foul_obj = FoulRequest()        
-        extend = lambda : extend_due_date(request=self.request, foul_data=foul_obj)
+        extend = lambda : api.extend_due_date(request=self.request, foul_data=foul_obj)
         response = extend()
         assert response == MOCK_DUEDATE
         
@@ -125,11 +129,3 @@ class TestApiFunctions(TestCase):
         foul_obj = FoulRequest()        
         extend_foul_due_date_mock.side_effect = errors.HttpError(500, "message")
         self.assertRaises(errors.HttpError, extend)
-
-
-
-
-
-
-
-        
