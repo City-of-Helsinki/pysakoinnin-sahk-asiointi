cf386dd6e99425da571d2d8be0759b63f676e11f
Juha Louhiranta <juha.louhiranta@anders.fi>
2024-09-19 15:36:00 +0300

    feat: strip api parameter values
    StripParser removes extra whitespace from
    request body (json) and query parameters.
    
    Refs: PSA-24

--- pysakoinnin_sahk_asiointi/urls.py
+++ pysakoinnin_sahk_asiointi/urls.py
@@ -8,6 +8,7 @@ from ninja.errors import HttpError
 from ninja.security import HttpBearer
 
 from api.api import router as api_router
+from api.parsers import StripParser
 from gdpr_api.views import router as gdrp_api_router
 
 
@@ -23,7 +24,12 @@ class AuthBearer(HttpBearer):
             raise HttpError(401, message=str(error))
 
 
-api = NinjaAPI(title="Pysäköinnin asiointi", version="1.0.0", auth=AuthBearer())
+api = NinjaAPI(
+    title="Pysäköinnin asiointi",
+    version="1.0.0",
+    auth=AuthBearer(),
+    parser=StripParser(),
+)
 
 api.add_router("/", api_router)
 api.add_router("/gdpr", gdrp_api_router)
