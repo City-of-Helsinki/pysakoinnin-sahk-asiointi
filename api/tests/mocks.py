5af8c4141043d37c7c39db4ae06e322af2613a51
Juha Louhiranta <juha.louhiranta@anders.fi>
2024-09-19 16:01:57 +0300

    refactor: run black on api tests
    Refs: PSA-24

--- api/tests/mocks.py
+++ api/tests/mocks.py
@@ -15,18 +15,18 @@ MOCK_FOUL = {
     "fouls": [
         {
             "description": "Pysäköinti ajosuunnan vastaisesti taajaman ulkopuolella. TLL 36 § 2 mom",
-            "additionalInfo": "12 metriä"
+            "additionalInfo": "12 metriä",
         },
         {
             "description": """Pysäköinti kiinteistölle johtavan ajotien kohdalle / siten,
                               että ajoneuvoliikenne kiinteistölle tai sieltä pois on oleellisesti
                               vaikeutunut. TLL 38 § 1 mom""",
-            "additionalInfo": "moikkaa "
+            "additionalInfo": "moikkaa ",
         },
         {
             "description": "Pysäköinti/Pysäyttäminen pysäkkialueelle. TLL 3 § 1 mom.,  75 §, Liikennemerkki E6",
-            "additionalInfo": "Ajoratamaalauksesta n. 2,0 m"
-        }
+            "additionalInfo": "Ajoratamaalauksesta n. 2,0 m",
+        },
     ],
     "invoiceSumText": "80,00 EUR",
     "openAmountText": "80,00 EUR",
@@ -38,7 +38,7 @@ MOCK_FOUL = {
     "attachments": [],
     "dueDateExtendable": False,
     "dueDateExtendableReason": 4,
-    "responseCode": 0
+    "responseCode": 0,
 }
 
 MOCK_TRANSFER = {
@@ -72,7 +72,7 @@ MOCK_TRANSFER = {
     "transferStatus": "Suoritettu",
     "transferReason": "Katualue varattu",
     "foulTypes": [],
-    "responseCode": 0
+    "responseCode": 0,
 }
 
 MOCK_DUEDATE = {
@@ -81,91 +81,83 @@ MOCK_DUEDATE = {
     "internalErrorDescription": "string",
     "dueDate": "2023-04-05T09:32:00",
     "dueDateExtendableReason": 0,
-    "responseCode": 0
+    "responseCode": 0,
 }
 
-MOCK_DUEDATE_REQUEST = {
-    "foul_number": 113148427,
-    "register_number": "HKR-999"
-}
+MOCK_DUEDATE_REQUEST = {"foul_number": 113148427, "register_number": "HKR-999"}
 
 MOCK_ATV_DOCUMENT_RESPONSE = {
-  "count": 1,
-  "next": None,
-  "previous": None,
-  "results": [
-    {
-      "id": "4y11fb6c-8b25-4c3c-9ea5-e62b976667e1",
-      "created_at": "2023-06-02T13:48:59.258113+03:00",
-      "updated_at": "2023-08-03T17:38:49.303800+03:00",
-      "status": {
-        "value": "received",
-        "status_display_values": {},
-        "timestamp": "2023-06-02T13:54:20.738979+03:00",
-        "activities": []
-      },
-      "status_histories": [
+    "count": 1,
+    "next": None,
+    "previous": None,
+    "results": [
         {
-          "value": "received",
-          "status_display_values": {},
-          "timestamp": "2023-06-02T13:54:20.738979+03:00",
-          "activities": []
-        },
-        {
-          "value": "sent",
-          "status_display_values": {},
-          "timestamp": "2023-06-02T13:48:59.269477+03:00",
-          "activities": []
+            "id": "4y11fb6c-8b25-4c3c-9ea5-e62b976667e1",
+            "created_at": "2023-06-02T13:48:59.258113+03:00",
+            "updated_at": "2023-08-03T17:38:49.303800+03:00",
+            "status": {
+                "value": "received",
+                "status_display_values": {},
+                "timestamp": "2023-06-02T13:54:20.738979+03:00",
+                "activities": [],
+            },
+            "status_histories": [
+                {
+                    "value": "received",
+                    "status_display_values": {},
+                    "timestamp": "2023-06-02T13:54:20.738979+03:00",
+                    "activities": [],
+                },
+                {
+                    "value": "sent",
+                    "status_display_values": {},
+                    "timestamp": "2023-06-02T13:48:59.269477+03:00",
+                    "activities": [],
+                },
+            ],
+            "type": "",
+            "human_readable_type": {},
+            "service": "Pysäköinnin Sähköinen Asiointi",
+            "user_id": "uuid",
+            "transaction_id": "113148474",
+            "business_id": "",
+            "tos_function_id": "12345",
+            "tos_record_id": "12345",
+            "metadata": {"foulNumber": "113148474", "registerNumber": "hkr-999"},
+            "content": {
+                "foulNumber": 113148474,
+                "transferNumber": None,
+                "folderID": None,
+                "ssn": "210281-9988",
+                "firstName": "Nordea",
+                "lastName": "Demo",
+                "email": "test@gmail.fi",
+                "mobilePhone": "+58254545414",
+                "bic": None,
+                "iban": "FI90 1345 1234 1234 12",
+                "authorRole": 2,
+                "address": {
+                    "addressLine1": None,
+                    "addressLine2": None,
+                    "streetAddress": "katu 8",
+                    "postCode": "00580",
+                    "postOffice": "jhelksini",
+                    "countryName": None,
+                },
+                "description": "description",
+                "attachments": [],
+                "type": 0,
+                "sendDecisionViaEService": True,
+                "metadata": {"foulNumber": "113148474", "registerNumber": "hkr-999"},
+            },
+            "draft": False,
+            "locked_after": None,
+            "deletable": False,
+            "attachments": [],
         }
-      ],
-      "type": "",
-      "human_readable_type": {},
-      "service": "Pysäköinnin Sähköinen Asiointi",
-      "user_id": "uuid",
-      "transaction_id": "113148474",
-      "business_id": "",
-      "tos_function_id": "12345",
-      "tos_record_id": "12345",
-      "metadata": {
-        "foulNumber": "113148474",
-        "registerNumber": "hkr-999"
-      },
-      "content": {
-        "foulNumber": 113148474,
-        "transferNumber": None,
-        "folderID": None,
-        "ssn": "210281-9988",
-        "firstName": "Nordea",
-        "lastName": "Demo",
-        "email": "test@gmail.fi",
-        "mobilePhone": "+58254545414",
-        "bic": None,
-        "iban": "FI90 1345 1234 1234 12",
-        "authorRole": 2,
-        "address": {
-          "addressLine1": None,
-          "addressLine2": None,
-          "streetAddress": "katu 8",
-          "postCode": "00580",
-          "postOffice": "jhelksini",
-          "countryName": None
-        },
-        "description": "description",
-        "attachments": [],
-        "type": 0,
-        "sendDecisionViaEService": True,
-        "metadata": {
-          "foulNumber": "113148474",
-          "registerNumber": "hkr-999"
-        }
-      },
-      "draft": False,
-      "locked_after": None,
-      "deletable": False,
-      "attachments": []
-    }
-  ]
+    ],
 }
+
 
 class MockResponse:
     def __init__(self, status_code, json_data):
--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -1,16 +1,27 @@
 from unittest.mock import patch
 from django.test import TestCase
-from api.tests.mocks import MockResponse, MOCK_FOUL, MOCK_TRANSFER, MOCK_ATV_DOCUMENT_RESPONSE, MOCK_DUEDATE
+from api.tests.mocks import (
+    MockResponse,
+    MOCK_FOUL,
+    MOCK_TRANSFER,
+    MOCK_ATV_DOCUMENT_RESPONSE,
+    MOCK_DUEDATE,
+)
 from api import api
 from ninja import Schema, errors
+
 
 # These classes are based on schemas defined in api.schemas
 class User:
     def __init__(self, uuid):
         self.uuid = uuid
+
+
 class Request:
     def __init__(self, user):
         self.user = user
+
+
 class Address(Schema):
     addressLine1: str = "string"
     addressLine2: str = "string"
@@ -18,9 +29,12 @@ class Address(Schema):
     postCode: str = "string"
     postOffice: str = "string"
     countryName: str = "string"
+
+
 class Metadata(Schema):
     lang: str = "fi"
     email: str = "testing@email.com"
+
 
 class Objection(Schema):
     foulNumber: int = 1
@@ -40,37 +54,44 @@ class Objection(Schema):
     sendDecisionViaEService: bool = True
     metadata: dict = Metadata().dict()
     attachments: list = []
+
+
 class FoulRequest(Schema):
     foul_number: str = "fi"
     register_number: str = "AB123"
     metadata: dict = Metadata().dict()
+
 
 class TestApiFunctions(TestCase):
     def setUp(self):
         self.user = User("fakeid")
         self.request = Request(user=self.user)
 
-    @patch('api.views.PASIHandler.get_foul_data')
+    @patch("api.views.PASIHandler.get_foul_data")
     def test_get_foul_data(self, get_foul_data_mock):
         get_foul_data_mock.return_value = MockResponse(200, MOCK_FOUL)
 
-        result = api.get_foul_data(request=None,
-                                   foul_number=MOCK_FOUL["foulNumber"],
-                                   register_number=MOCK_FOUL["registerNumber"])
+        result = api.get_foul_data(
+            request=None,
+            foul_number=MOCK_FOUL["foulNumber"],
+            register_number=MOCK_FOUL["registerNumber"],
+        )
 
         assert result == MOCK_FOUL
 
-    @patch('api.views.PASIHandler.get_transfer_data')
+    @patch("api.views.PASIHandler.get_transfer_data")
     def test_get_transfer_data(self, get_transfer_data_mock):
         get_transfer_data_mock.return_value = MockResponse(200, MOCK_TRANSFER)
 
-        result = api.get_transfer_data( request=None,
-                                        transfer_number=MOCK_TRANSFER["transferNumber"],
-                                        register_number=MOCK_TRANSFER["registerNumber"])
+        result = api.get_transfer_data(
+            request=None,
+            transfer_number=MOCK_TRANSFER["transferNumber"],
+            register_number=MOCK_TRANSFER["registerNumber"],
+        )
 
         assert result == MOCK_TRANSFER
 
-    @patch('api.views.ATVHandler.get_documents')
+    @patch("api.views.ATVHandler.get_documents")
     def test_get_atv_documents(self, get_documents_mock):
         get_documents_mock.return_value = MockResponse(200, MOCK_ATV_DOCUMENT_RESPONSE)
 
@@ -78,24 +99,26 @@ class TestApiFunctions(TestCase):
 
         assert result.json() == MOCK_ATV_DOCUMENT_RESPONSE
 
-    @patch('api.views.ATVHandler.get_document_by_transaction_id')
+    @patch("api.views.ATVHandler.get_document_by_transaction_id")
     def test_get_document_by_transaction_id(self, get_document_by_transaction_id_mock):
-        get_document_by_transaction_id_mock.return_value = MockResponse(200, MOCK_ATV_DOCUMENT_RESPONSE)
+        get_document_by_transaction_id_mock.return_value = MockResponse(
+            200, MOCK_ATV_DOCUMENT_RESPONSE
+        )
 
         randomId = 12345
         result = api.get_document_by_transaction_id(request=None, id=randomId)
 
         assert result.json() == MOCK_ATV_DOCUMENT_RESPONSE
 
-    @patch('api.views.ATVHandler.add_document')
-    @patch('api.views.PASIHandler.save_objection')
+    @patch("api.views.ATVHandler.add_document")
+    @patch("api.views.PASIHandler.save_objection")
     def test_save_objection(self, save_objection_mock, add_document_mock):
         # status code is only interesting value here
         add_document_mock.return_value = MockResponse(200, {})
         save_objection_mock.return_value = MockResponse(200, {})
 
         objection = Objection()
-        save_obj = lambda : api.save_objection(request=self.request, objection=objection)
+        save_obj = lambda: api.save_objection(request=self.request, objection=objection)
         self.assertEqual(200, save_obj())
 
         # Raise httperror 422
@@ -109,14 +132,14 @@ class TestApiFunctions(TestCase):
         objection.metadata = "incorrect metadata"
         self.assertRaises(errors.HttpError, save_obj)
 
-    @patch('api.views.ATVHandler.add_document')
-    @patch('api.views.PASIHandler.extend_foul_due_date')
-    def test_extend_due_date(self,extend_foul_due_date_mock, add_document_mock):
+    @patch("api.views.ATVHandler.add_document")
+    @patch("api.views.PASIHandler.extend_foul_due_date")
+    def test_extend_due_date(self, extend_foul_due_date_mock, add_document_mock):
         add_document_mock.return_value = MockResponse(200, {})
         extend_foul_due_date_mock.return_value = MockResponse(200, MOCK_DUEDATE)
 
         foul_obj = FoulRequest()
-        extend = lambda : api.extend_due_date(request=self.request, foul_data=foul_obj)
+        extend = lambda: api.extend_due_date(request=self.request, foul_data=foul_obj)
 
         # NOTE to enable this, sending emails needs to be mocked or skipped in test runs.
         # You could also use MAILER_EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend" in test runs
