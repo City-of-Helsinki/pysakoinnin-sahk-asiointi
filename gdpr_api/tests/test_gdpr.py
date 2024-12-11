8cf69573cb00dcb6d51b5df11b7dd26748eb0fc3
Matti Eiden <matti.eiden@anders.com>
2024-12-11 18:34:24 +0200

    fix: make GDPR api more compliant with standard
    * remove custom JWTAuth implementation, use helusers
      RequestJWTAuthentication
    * return 403 instead of 401 in permission checks
    * add tests

---
+++ gdpr_api/tests/test_gdpr.py
@@ -0,0 +1,106 @@
+import uuid
+from unittest import mock
+
+import pytest
+import requests_mock
+from django.conf import settings
+from django.urls import reverse
+
+from gdpr_api.tests.conftest import get_api_token_for_user_with_scopes
+from pysakoinnin_sahk_asiointi.models import User
+
+
+def _gdpr_action(method, scope, user, query_uuid=None):
+    if not query_uuid:
+        query_uuid = user.uuid
+    with requests_mock.Mocker() as req_mock:
+        gdpr_profile_url = reverse("api-1.0.0:get_user_info", kwargs={"user_id": query_uuid})
+        auth_header = get_api_token_for_user_with_scopes(
+            user.uuid, [scope], req_mock
+        )
+        return method(gdpr_profile_url, headers={"Authorization": auth_header})
+
+
+def _gdpr_query(api_client, user, query_uuid=None):
+    return _gdpr_action(api_client.get, settings.GDPR_API_QUERY_SCOPE, user, query_uuid)
+
+
+def _gdpr_delete(api_client, user, query_uuid=None):
+    return _gdpr_action(api_client.delete, settings.GDPR_API_DELETE_SCOPE, user, query_uuid)
+
+
+@pytest.fixture
+def user():
+    return User(uuid=uuid.uuid4())
+
+
+@pytest.mark.django_db
+def test_gdpr_query_wrong_user(client, user):
+    assert _gdpr_query(client, user, uuid.uuid4()).status_code == 403
+
+
+@pytest.mark.django_db
+def test_gdpr_query_user_with_no_documents(client, user):
+    with mock.patch("api.views.ATVHandler.get_documents") as get_documents_mock:
+        get_documents_mock.return_value = {"results": []}
+        assert _gdpr_query(client, user).status_code == 204
+
+
+@pytest.mark.django_db
+def test_gdpr_query_user_with_documents(client, user):
+    with mock.patch("api.views.ATVHandler.get_documents") as get_documents_mock:
+        get_documents_mock.return_value = {
+            "results": [
+                {"content": {
+                    "object": {"key1": "value1"},
+                    "list": [{"key2": "value2"}],
+                    "key3": "value3"
+                }}
+            ]
+        }
+        response = _gdpr_query(client, user)
+        assert response.status_code == 200
+        assert response.json() == {
+            'children': [{
+                'children': [{
+                    'children': [{'key': 'key1', 'value': 'value1'}],
+                    'key': 'object'
+                }, {
+                    'children': [{'key': 'key2', 'value': 'value2'}],
+                    'key': 'list'
+                }, {
+                    'key': 'key3',
+                    'value': 'value3'
+                }],
+                'key': 'CONTENT'
+            }],
+            'key': 'RESULTS'}
+
+
+@pytest.mark.django_db
+def test_gdpr_delete_wrong_user(client, user):
+    assert _gdpr_delete(client, user, uuid.uuid4()).status_code == 403
+
+
+@pytest.mark.django_db
+def test_gdpr_delete_user_with_no_documents(client, user):
+    with mock.patch("api.views.ATVHandler.get_documents") as get_documents_mock:
+        get_documents_mock.return_value = {"results": []}
+        assert _gdpr_delete(client, user).status_code == 204
+
+
+@pytest.mark.django_db
+def test_gdpr_delete_user_with_documents(client, user):
+    with mock.patch("api.views.ATVHandler.get_documents") as get_documents_mock:
+        get_documents_mock.return_value = {"results": [{}]}
+        assert _gdpr_delete(client, user).status_code == 403
+
+
+@pytest.mark.django_db
+def test_gdpr_delete_with_query_scope(client, user):
+    assert _gdpr_action(client.delete, settings.GDPR_API_QUERY_SCOPE, user).status_code == 403
+
+
+@pytest.mark.django_db
+def test_gdpr_query_with_delete_scope(client, user):
+    assert _gdpr_action(client.get, settings.GDPR_API_DELETE_SCOPE, user).status_code == 403
--- gdpr_api/views.py
+++ gdpr_api/views.py
@@ -1,50 +1,26 @@
 from django.conf import settings
-from django.http import HttpRequest, JsonResponse, HttpResponse
-from helusers.jwt import JWT
-from helusers.oidc import OIDCConfig
+from django.http import JsonResponse, HttpResponse, HttpRequest
 from ninja import Router
 from ninja.errors import HttpError
-from ninja.security import HttpBearer
 
 from api.views import ATVHandler
+
+
+def check_gdpr_permission(request: HttpRequest, user_id: str, scope: str) -> None:
+    if request.user.uuid and str(request.user.uuid) != user_id:
+        raise HttpError(403, message="Permission denied")
+
+    if not request.auth.has_api_scopes(scope):
+        raise HttpError(403, message=f"Token missing required scope {scope}")
+
 
 router = Router()
 
 
-class JWTAuth(HttpBearer):
-    def __init__(self, required_scope: str):
-        self.required_scope = required_scope
-        super().__init__()
-
-    def authenticate(self, request: HttpRequest, token: str):
-        oidc_config = OIDCConfig(settings.GDPR_API_ISSUER)
-
-        jwt = JWT(token)
-        try:
-            issuer = jwt.issuer
-        except KeyError:
-            raise HttpError(401, message='Required "iss" claim is missing.')
-
-        if issuer != settings.GDPR_API_ISSUER:
-            raise HttpError(401, message="Unknown JWT issuer {}.".format(issuer))
-
-        try:
-            jwt.validate(oidc_config.keys(), settings.GDPR_API_AUDIENCE)
-        except Exception:
-            raise HttpError(401, message="JWT verification failed.")
-
-        api_scopes = jwt.claims.get(settings.OIDC_API_TOKEN_AUTH["API_AUTHORIZATION_FIELD"], [])
-        if self.required_scope not in api_scopes:
-            print(self.required_scope)
-            raise HttpError(401, message="No suitable API scope found")
-
-        return jwt.claims.get("sub")
-
-
-@router.get('/{user_id}', tags=["GDPR API"], auth=JWTAuth(required_scope=settings.GDPR_API_QUERY_SCOPE))
+@router.get('/{user_id}', tags=["GDPR API"])
 def get_user_info(request, user_id: str):
-    if not request.auth or request.auth != user_id:
-        raise HttpError(401, message="Unauthorised")
+    check_gdpr_permission(request, user_id, settings.GDPR_API_QUERY_SCOPE)
+
     req = ATVHandler.get_documents(user_id)
 
     if len(req['results']) <= 0:
@@ -97,10 +73,9 @@ def get_user_info(request, user_id: str):
     return return_obj
 
 
-@router.delete('/{user_id}', tags=["GDPR API"], auth=JWTAuth(required_scope=settings.GDPR_API_DELETE_SCOPE))
+@router.delete('/{user_id}', tags=["GDPR API"])
 def delete_user_info(request, user_id: str):
-    if not request.auth or request.auth != user_id:
-        raise HttpError(401, message="Unauthorised")
+    check_gdpr_permission(request, user_id, settings.GDPR_API_DELETE_SCOPE)
 
     req = ATVHandler.get_documents(user_id)
 
